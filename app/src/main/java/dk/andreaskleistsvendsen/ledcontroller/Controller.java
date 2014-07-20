ef354f33344762212e96d6867f3d0f2da1016dce
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2014-07-20 12:11:50 +0200

    Add button handlers and dummy actions for clicks

--- app/src/main/java/dk/andreaskleistsvendsen/ledcontroller/Controller.java
+++ app/src/main/java/dk/andreaskleistsvendsen/ledcontroller/Controller.java
@@ -4,14 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Controller extends Activity {
+
+    private Button onButton_;
+    private Button offButton_;
+    private TextView statusLabel_;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+        statusLabel_ = (TextView) findViewById(R.id.statusLabel);
+        addButtonListeners();
     }
 
 
@@ -32,5 +42,22 @@ public class Controller extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addButtonListeners() {
+        onButton_ = (Button) findViewById(R.id.onButton);
+        offButton_ = (Button) findViewById(R.id.offButton);
+
+        setStringOnClick_(onButton_, R.string.turning_on);
+        setStringOnClick_(offButton_, R.string.turning_off);
+    }
+
+    private void setStringOnClick_(Button button, final int stringId) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                statusLabel_.setText(stringId);
+            }
+        });
     }
 }
