3938b37d58ccdcb3fe7bb78e8149f54f1f600624
Andreas Kleist Svendsen <andreaskleist@gmail.com>
2014-10-14 19:11:04 +0200

    Refactoring, removing unused method, renaming addListener2_ to addListener_

--- app/src/main/java/dk/andreaskleistsvendsen/ledcontroller/Controller.java
+++ app/src/main/java/dk/andreaskleistsvendsen/ledcontroller/Controller.java
@@ -5,14 +5,12 @@ import android.app.FragmentTransaction;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.IOException;
 import java.net.UnknownHostException;
 
 
@@ -72,12 +70,7 @@ public class Controller extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void addListener_(int id, View.OnClickListener listener) {
-        Button btn = (Button) findViewById(id);
-        btn.setOnClickListener(listener);
-    }
-
-    private void addListener2_(int id, final String msg, final Runnable runnable) {
+    private void addListener_(int id, final String msg, final Runnable runnable) {
         Button btn = (Button) findViewById(id);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,38 +82,44 @@ public class Controller extends Activity {
     }
 
     void addButtonListeners() {
-        addListener2_(R.id.btnAllOff, "Turning all off", new Runnable(){
-            @Override public void run() {
+        addListener_(R.id.btnAllOff, "Turning all off", new Runnable() {
+            @Override
+            public void run() {
                 bridge_.allOff();
             }
         });
 
-        addListener2_(R.id.btnAllOn, "Turning all on", new Runnable(){
-            @Override public void run() {
+        addListener_(R.id.btnAllOn, "Turning all on", new Runnable() {
+            @Override
+            public void run() {
                 bridge_.allOn();
             }
         });
 
-        addListener2_(R.id.btnWhiteOff, "Turning white off", new Runnable(){
-            @Override public void run() {
+        addListener_(R.id.btnWhiteOff, "Turning white off", new Runnable() {
+            @Override
+            public void run() {
                 bridge_.whiteOff();
             }
         });
 
-        addListener2_(R.id.btnWhiteOn, "Turning white on", new Runnable(){
-            @Override public void run() {
+        addListener_(R.id.btnWhiteOn, "Turning white on", new Runnable() {
+            @Override
+            public void run() {
                 bridge_.whiteOn();
             }
         });
 
-        addListener2_(R.id.btnRGBOff, "Turning RGB off", new Runnable(){
-            @Override public void run() {
+        addListener_(R.id.btnRGBOff, "Turning RGB off", new Runnable() {
+            @Override
+            public void run() {
                 bridge_.rgbOff();
             }
         });
 
-        addListener2_(R.id.btnRGBOn, "Turning RGB on", new Runnable(){
-            @Override public void run() {
+        addListener_(R.id.btnRGBOn, "Turning RGB on", new Runnable() {
+            @Override
+            public void run() {
                 bridge_.rgbOn();
             }
         });
